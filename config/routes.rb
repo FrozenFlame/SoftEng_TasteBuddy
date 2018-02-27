b637a3ff8e83c8f1b46e8df736f9b6b6b9369b10
FF-Desktop <metriod931@yahoo.com>
2018-02-28 00:00:04 +0800

    minor patch

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'products/search' => 'products#search', as: 'search_products'
+  get 'orders/search' => 'orders#search', as: 'search_orders'
   get 'ordersummary' => 'ordersummary#index'
   
 end
