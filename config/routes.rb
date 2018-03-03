5885a60d8d17667005064957e538a479d494f566
FF-Desktop <metriod931@yahoo.com>
2018-03-03 20:13:25 +0800

    basic login working

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # routes notes:
   # formats:
   # root 
+  # regular route format is get '<pageName>' => '<controller>#<function>'
+  # get '<THIS PART>' is incidentally the name of the link usually (e.g. in html.erb) this is the link_to "<Text that user sees>", <THIS PART>_path
+  # this is also (probably) the name of the method found @ the controller 
+  # finally, you can give these custom names if you wish to be pointed at easier (as: part)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'products/search' => 'products#search', as: 'search_products'
@@ -12,7 +16,8 @@ Rails.application.routes.draw do
   
   # signup routes
   get 'users' => 'users#new', as: 'signup'
-
+  
+  get 'logout' => 'sessions#logout'
   #temporary root: products, it should be front page
   root 'main_page#index'
 
