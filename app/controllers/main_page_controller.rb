a6e5ffa72b63141d6bdbca35018a476ee910fab0
FF-Desktop <metriod931@yahoo.com>
2018-03-03 22:46:00 +0800

    Main_page pulls from db
    images still not done

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,4 +1,16 @@
 class MainPageController < ApplicationController
   def index
+    # we're gonna need to have some flags here to determine what results will appear.
+    @products = Product.all
   end
+
+
+  def shorten(str)
+      if str.length > 50
+        return str[0..50] +"..."
+      else
+        return str  
+      end
+    end
+    helper_method :shorten
 end
