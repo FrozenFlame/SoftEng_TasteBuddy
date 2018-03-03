6b8cc354b032b0db1af75f28b4bd9acfe6d199cd
FF-Desktop <metriod931@yahoo.com>
2018-03-03 21:42:05 +0800

    Product form now a dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User
   field :password, type: String             # password to validate log in
   field :orders, type: Array   # filled with order codes
   field :cart, type: Array     # filled with what they have in their shopping cart, still coordinated in the database.
+  field :isAdmin, type: Boolean, default: false
   # field :_id, type: String, default: -> { prodCode.to_s.parameterize } # this makes it so the link above would be reflected here BUT only if they have been newly created
   
 end
